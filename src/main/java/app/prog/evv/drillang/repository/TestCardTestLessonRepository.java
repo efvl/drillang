58354c4150817a8cfe250775f5498435ca021606
ef vl <efvl2022@gmail.com>
2023-03-27 20:01:03 +0200

    create repository to TestCardRestLesson entity

---
+++ src/main/java/app/prog/evv/drillang/repository/TestCardTestLessonRepository.java
@@ -0,0 +1,9 @@
+package app.prog.evv.drillang.repository;
+
+import app.prog.evv.drillang.entity.TestCardTestLessonEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestCardTestLessonRepository extends BaseJpaRepository<TestCardTestLessonEntity, Long>{
+
+}
